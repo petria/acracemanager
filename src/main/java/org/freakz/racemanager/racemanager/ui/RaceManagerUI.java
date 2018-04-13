dc1f27207e6d22da58a31507d795aa8a053ce0a0
Petri Airio <petri.j.airio@gmail.com>
2018-04-13 09:59:34 +0300

    Automatic Push

--- src/main/java/org/freakz/racemanager/racemanager/ui/RaceManagerUI.java
+++ src/main/java/org/freakz/racemanager/racemanager/ui/RaceManagerUI.java
@@ -5,7 +5,6 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewDisplay;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.shared.communication.PushMode;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.spring.annotation.SpringViewDisplay;
 import com.vaadin.spring.navigator.SpringViewProvider;
@@ -21,7 +20,7 @@ import org.freakz.racemanager.racemanager.ui.view.DefaultView;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Theme("valo")
-@Push(PushMode.MANUAL)
+@Push
 @SpringUI
 @SpringViewDisplay
 public class RaceManagerUI extends UI implements ViewDisplay, Broadcaster.BroadcastListener {
